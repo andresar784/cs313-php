2aff4e8118c9159706b536ed5c376411b15a8067
Andres Rodriguez <rod16015@byui.edu>
2019-05-14 01:29:10 -0300

    Added minor changes

--- web/week3/process.php
+++ web/week3/process.php
@@ -77,7 +77,9 @@ if (! isset ( $_SESSION ['add_cart'] )) {
 <div class=" text-white p-5">
         <div class="p-3 text-white text-center">
           <h2>Shopping Cart</h2>
+          <?php
           var_dump("add_cart");
+          ?>
           <button class="btn btn-secondary" id="removeAll">Remove all items</button>
 		  <a class="btn-secondary" href="checkout.php">Continue to checkout</a>
         </div>
