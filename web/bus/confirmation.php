1656473f839f67992cbf49eb77b847332940b46d
Andres Rodriguez <rod16015@byui.edu>
2019-05-30 22:54:22 -0300

    Added minot changes

--- web/bus/confirmation.php
+++ web/bus/confirmation.php
@@ -53,7 +53,7 @@
 	<div class="p-3 text-white text-center">
 		<div class="col-xs-6">
     <?php
-    var_dump(isset($from));?>
+    var_dump(empty($from));?>
     <?php echo $from;?>
 		<p> You are travelling from: <?php echo $from;?> </p>
     <p> To: <?php echo $to;?></p>
