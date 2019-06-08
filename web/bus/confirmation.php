d2d68375a8f63f0c72556bbd2d8419b7566a9075
Andres Rodriguez <rod16015@byui.edu>
2019-06-08 09:30:36 -0300

    Added minor changes

--- web/bus/confirmation.php
+++ web/bus/confirmation.php
@@ -61,6 +61,7 @@ session_start();
 		<dt> You are travelling from: <?php echo $from?> </dt><br>
     <dt> To: <?php echo $to;?></dt><br>
 		<dt> Total of passengers are: <?php echo $quantity;?> </dt><br>
+    <dt> Total ammount is: </dt>
     <form action="index.php">
     <button  type="submit"  class="btn btn-primary btn-lg" >Return</button>
     </form>
