fec4f95427cee70065c15a5116718523e02830c3
Andres Rodriguez <rod16015@byui.edu>
2019-06-08 13:18:09 -0300

    Added minor changes

--- web/bus/confirmation.php
+++ web/bus/confirmation.php
@@ -76,6 +76,48 @@ session_start();
 		</div>
  </div><br><br>
 
+ <script>
+ function calculatePrice(){  
+  var q = "<?=$quantity ?>";
+  var price;
+  var from = "<?=$from ?>";
+  var to = "<?=$to ?>";
+
+  if (from == "Montevideo" && to == "Durazno" || from == "Durazno" && to == "Montevideo"){
+    price = 180 * q;
+  }
+  else if (from == "Montevideo" && to == "Paso de los Toros" || from == "Paso de los Toros" && to == "Montevideo"){
+    price = 250 * q;
+  }
+  else if (from == "Montevideo" && to == "Tacuarembo" || from == "Rivera" && to == "Montevideo" ){
+    price = 390 * q;
+  }
+  else if(from == "Montevideo" && to == "Rivera" || from == "Rivera" && to == "Montevideo"){
+    price = 500 * q;
+  }
+  else if(from == "Durazno" && to == "Paso de los Toros" || from == "Paso de los Toros" && to == "Durazno"){
+    price = 70 * q;
+  }
+  else if(from == "Durazno" && to == "Tacuarembo" || from == "Tacuarembo" && to == "Durazno"){
+    price = 180 * q;
+  }
+  else if(from == "Durazno" && to == "Rivera" || from == "Rivera" && to == "Durazno"){
+    price = 320 * q;
+  }
+  else if(from == "Paso de los Toros" && to == "Tacuarembo" || from == "Tacuarembo" && to == "Paso de los Toros"){
+    price = 140 * q;
+  }
+  else if(from == "Paso de los Toros" && to == "Rivera" || from == "Rivera" && to == "Paso de los Toros"){
+    price = 250 * q;
+  }
+  else if(from == "Tacuarembo" && to == "Rivera" || from == "Rivera" && to == "Tacuarembo"){
+    price = 110 * q;
+  }
+  document.getElementById("showprice").innerHTML = price;
+}
+
+</script>
+
 
 
 <footer class="container-fluid text-center">
