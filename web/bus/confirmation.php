8ea471144cd294c0363e15eff3d3a9e03283d7bc
Andres Rodriguez <rod16015@byui.edu>
2019-06-08 10:52:06 -0300

    Added minor changes

--- web/bus/confirmation.php
+++ web/bus/confirmation.php
@@ -60,7 +60,7 @@ session_start();
     <dt> To: <?php echo $to;?></dt><br>
 		<dt> Total of passengers are: <?php echo $quantity;?> </dt><br>
     <dt> Total amount is: <p id="showprice">0</p> </dt><br>
-    <button onclick="calculatePrice()">Calculate Price</button>
+    <button onclick="calculatePrice(<?php echo $from;?>)">Calculate Price</button>
     <form action="index.php">
     <button  type="submit"  class="btn btn-primary btn-lg" >Return</button>
     </form>
