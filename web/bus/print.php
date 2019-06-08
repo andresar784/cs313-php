1043626f6f525c1b955d33cd9186668c90410083
Andres Rodriguez <rod16015@byui.edu>
2019-06-07 23:45:45 -0300

    Added minor changes

--- web/bus/print.php
+++ web/bus/print.php
@@ -1,14 +1,11 @@
 <?php
-require('fpdf.php');
+require('fpdf/fpdf.php');
 
 $pdf = new FPDF();
 $pdf->AddPage();
 $pdf->SetFont('Arial','B',16);
 $pdf->Cell(40,10,'Hello World!');
-$pdf->Output('print.pdf', "I");
+$pdf->Output();
 
 ?>
 
-<?php
-echo "Hello World";
-?>
