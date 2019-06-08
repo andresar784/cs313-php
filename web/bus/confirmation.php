7e348511b9bbe9f0cf82555bc0cd7f5ea0d814c5
Andres Rodriguez <rod16015@byui.edu>
2019-06-08 15:24:58 -0300

    Added minor changes

--- web/bus/confirmation.php
+++ web/bus/confirmation.php
@@ -65,6 +65,7 @@ session_start();
     <dt> To: <?php echo $to;?></dt><br>
 		<dt> Total of passengers are: <?php echo $quantity;?> </dt><br>
     <dt> Total amount is (in dollars): <?php echo $price;?><br>
+    <button onclick="calculatePrice()">calculate Price<</button>
     <form action="index.php">
     <button  type="submit"  class="btn btn-primary btn-lg" >Return</button>
     </form>
@@ -77,6 +78,52 @@ session_start();
     </div>
 		</div>
  </div><br><br>
+
+ <script>
+ function calculatePrice(){  
+   var q = "<?=$quantity ?>";
+  var price;
+  var from = "<?=$from ?>";
+  var to = "<?=$to ?>";
+
+  if (from == "Montevideo" && to == "Durazno" || from == "Durazno" && to == "Montevideo"){
+    price = 180 * q;
+  }
+  else if (from == "Montevideo" && to == "Paso de los Toros" || from == "Paso de los Toros" && to == "Montevideo"){
+    price = 250 * q;
+  }
+  else if (from == "Montevideo" && to == "Tacuarembo" || from == "Rivera" && to == "Montevideo" ){
+    price = 390 * q;
+  }
+  else if(from == "Montevideo" && to == "Rivera" || from == "Rivera" && to == "Montevideo"){
+    price = 500 * q;
+  }
+  else if(from == "Durazno" && to == "Paso de los Toros" || from == "Paso de los Toros" && to == "Durazno"){
+    price = 70 * q;
+  }
+  else if(from == "Durazno" && to == "Tacuarembo" || from == "Tacuarembo" && to == "Durazno"){
+    price = 180 * q;
+  }
+  else if(from == "Durazno" && to == "Rivera" || from == "Rivera" && to == "Durazno"){
+    price = 320 * q;
+  }
+  else if(from == "Paso de los Toros" && to == "Tacuarembo" || from == "Tacuarembo" && to == "Paso de los Toros"){
+    price = 140 * q;
+  }
+  else if(from == "Paso de los Toros" && to == "Rivera" || from == "Rivera" && to == "Paso de los Toros"){
+    price = 250 * q;
+  }
+  else if(from == "Tacuarembo" && to == "Rivera" || from == "Rivera" && to == "Tacuarembo"){
+    price = 110 * q;
+  }
+	document.getElementById("showprice").attribute = price;
+	
+	alert(price);
+
+}
+
+</script>
+ 
 
  
 
