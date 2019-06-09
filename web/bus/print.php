4d0132d3e314db82b692360de9456ff6810b2b0f
Andres Rodriguez <rod16015@byui.edu>
2019-06-08 23:12:11 -0300

    Added minor changes

--- web/bus/print.php
+++ web/bus/print.php
@@ -1,11 +1,18 @@
 <?php
+session_start();
+$from = $_SESSION["from"];
+$to = $_SESSION["to"];
+$quantity = $_SESSION["quantity"];
+$firstname = $_SESSION["firstname"];
+$lastname = $_SESSION["lastname"];
+
 include 'template.php';
 
 $pdf = new PDF();
 $pdf->AliasNbPages();
 $pdf->AddPage();
 
-$pdf->SetFillColor(232,232,232);
+$pdf->SetFillColor(102,178,255);
 $pdf->SetFont('Arial','I',12);
 $pdf->Cell(30,6,'Name',1,0,'C',1);
 $pdf->Cell(30,6,'Last Name',1,0,'C',1);
@@ -14,6 +21,11 @@ $pdf->Cell(30,6,'To',1,0,'C',1);
 $pdf->Cell(30,6,'Tickets',1,0,'C',1);
 
 $pdf->SetFont('Arial','',10);
+$pdf->Cell(30,6,'$firstname',1,0,'C',1);
+$pdf->Cell(30,6,'Last Name',1,0,'C',1);
+$pdf->Cell(30,6,'From',1,0,'C',1);
+$pdf->Cell(30,6,'To',1,0,'C',1);
+$pdf->Cell(30,6,'Tickets',1,0,'C',1);
 
 
 $pdf->Output();
