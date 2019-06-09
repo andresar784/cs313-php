9cc09720577ca497f8fc058d1622d7ce7a095a9b
Andres Rodriguez <rod16015@byui.edu>
2019-06-08 22:31:05 -0300

    Added minor changes

--- web/bus/print.php
+++ web/bus/print.php
@@ -1,13 +1,23 @@
 <?php
+require 'conexion.php';
 
 
-require('fpdf/fpdf.php');
 
-$pdf = new FPDF();
+$pdf = new PDF();
+$pdf->AliasNbPages();
 $pdf->AddPage();
-$pdf->SetFont('Arial','B',16);
-$pdf->Cell(40,10,'¡Hola, Mundo!');
+
+$pdf->SetFillColor(232,232,232);
+$pdf->SetFont('Arial','B',12);
+$pdf->Cell(70,6,'ESTADO',1,0,'C',1);
+$pdf->Cell(20,6,'ID',1,0,'C',1);
+$pdf->Cell(70,6,'MUNICIPIO',1,1,'C',1);
+
+$pdf->SetFont('Arial','',10);
+
+
 $pdf->Output();
 ?>
+
 
 
