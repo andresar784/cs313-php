e560de76e1c303849a559bdefa42d7993c36d419
Andres Rodriguez <rod16015@byui.edu>
2019-06-08 22:52:14 -0300

    Added minor changes

--- web/bus/template.php
+++ web/bus/template.php
@@ -4,7 +4,7 @@
  class PDF extends FPDF{
     function Header()
     {
-        $this->Image('pictures/Sbl.png', 5, 5, 30);
+        $this->Image('pictures/Sbl.png', 5, 5, 35);
         $this->SetFont('Arial', 'B', '20');
         $this->Cell(30);
         $this->Cell(120,10,'Your tickets', 0,0,'C');
