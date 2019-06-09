62d41ebc03eb591108ef03db1fef367ac6eb83e8
Andres Rodriguez <rod16015@byui.edu>
2019-06-09 00:05:40 -0300

    Added minor changes

--- web/bus/print.php
+++ web/bus/print.php
@@ -21,6 +21,8 @@ $pdf->Cell(30,6,'From',1,0,'C',1);
 $pdf->Cell(30,6,'To',1,0,'C',1);
 $pdf->Cell(30,6,'Tickets',1,1,'C',1);
 
+$this->Ln(20);
+$pdf->SetFillColor(88,178,255);
 $pdf->SetFont('Arial','',10);
 $pdf->Cell(30,6,''.$firstname,1,0,'C',1);
 $pdf->Cell(30,6,''.$lastname,1,0,'C',1);
