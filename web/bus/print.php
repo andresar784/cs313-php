8212f34c3bddbfaa022ab4bb5bc98159bf74ade3
Andres Rodriguez <rod16015@byui.edu>
2019-06-08 22:59:18 -0300

    Added minor changes

--- web/bus/print.php
+++ web/bus/print.php
@@ -5,7 +5,7 @@ $pdf = new PDF();
 $pdf->AliasNbPages();
 $pdf->AddPage();
 
-
+$pdf->SetFillColor(232,232,232);
 $pdf->SetFont('Arial','B',12);
 $pdf->Cell(30,6,'Name',1,0,'C',1);
 $pdf->Cell(20,6,'Last Name',1,0,'C',1);
