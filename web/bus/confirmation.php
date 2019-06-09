f5251bd479453dd34c36374904653da6bc3e2c5a
Andres Rodriguez <rod16015@byui.edu>
2019-06-09 01:04:56 -0300

    Added minor changes

--- web/bus/confirmation.php
+++ web/bus/confirmation.php
@@ -121,24 +121,6 @@ session_start();
   }
 
   document.getElementById("showPrice").innerHTML = price;
-
-$(document).ready(function () {
-  createCookie("height", $(window).height(), "10");
-});
-
-function createCookie(name, value, days) {
-  var expires;
-  if (days) {
-    var date = new Date();
-    date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-    expires = "; expires=" + date.toGMTString();
-  } else {
-   expires = "";
-  }
-  document.cookie = escape(name) + "=" + escape(value) + expires + "; path=/";
-}
-  
-  
 }
 
 </script>
