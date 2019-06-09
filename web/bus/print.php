ac0bb5eb8e87cd68e3b80c5e17b23889166cea35
Andres Rodriguez <rod16015@byui.edu>
2019-06-08 21:56:46 -0300

    Added minor changes

--- web/bus/print.php
+++ web/bus/print.php
@@ -1,11 +1,18 @@
 <?php
-require('fpdf/fpdf.php');
 
-$pdf = new FPDF();
-$pdf->AddPage();
-$pdf->SetFont('Arial','B',16);
-$pdf->Cell(40,10,'Hello World!');
-$pdf->Output();
+    include('template.php');
+    $pdf = new PDF();
+    $pdf->Addpage();
+
+    $pdf->SetFont('Arial','B',12);
+    $pdf->Cell(30,6,'Name',1,0,'C',1);
+    $pdf->Cell(20,6,'How many tickets',1,0,'C',1);
+    $pdf->Cell(30,6,'From',1,0,'C',1);
+    $pdf->Cell(30,6,'To',1,0,'C',1);
+
+    
+
+
 
 ?>
 
